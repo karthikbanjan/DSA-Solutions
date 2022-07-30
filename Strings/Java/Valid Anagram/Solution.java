7a7e9fc2195351e03c1d8f6a4ca1d9f4bf5f7724
karthikbanjan <banjankarthik@gmail.com>
2022-07-30 11:24:56 +0530

    Anagram Solution Update

--- Strings/Java/Valid Anagram/Solution.java
+++ Strings/Java/Valid Anagram/Solution.java
@@ -3,6 +3,10 @@ import java.util.Arrays;
 class Solution {
 
     public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
         char[] sc = s.toCharArray();
         char[] tc = t.toCharArray();
 
